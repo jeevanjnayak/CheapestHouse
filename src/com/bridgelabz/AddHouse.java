074307e99385c87cd116bc8411126b3a0ac75445
Jeevan <jeevanjnayak@gmail.com>
2022-09-10 20:01:53 +0530

    Added: added a house to the list

---
+++ src/com/bridgelabz/AddHouse.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddHouse {
+    static List<House> houseList = new ArrayList<>()
+    public static void houseAdding(){
+        House house1 = new House("white house", "White", 6000000);
+        houseList.add(house1);
+
+    }
+}
